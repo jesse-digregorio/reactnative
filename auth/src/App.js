5be22ce38d8465cd3f29c01cbd87d30592d3118c
Jesse DiGregorio <jesse.digregorio@live.com>
2018-03-31 20:29:39 -0700

    beginning work

---
+++ auth/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
+
+class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+		apiKey: 'AIzaSyDF0vwz2hGjWgy22gzyA0XB_EzbmMlB_sc',
+		authDomain: 'authentication-rnc9.firebaseapp.com',
+		databaseURL: 'https://authentication-rnc9.firebaseio.com',
+		projectId: 'authentication-rnc9',
+		storageBucket: 'authentication-rnc9.appspot.com',
+		messagingSenderId: '320056014461'
+  		});
+	}
+
+	render() {
+		return (
+			<View>
+				<Header headerText="Authentication" />
+				<LoginForm />
+			</View>
+			);
+	}
+}
+
+export default App;
