081b0f139e51e430721ce01b6737be26524de4a9
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-02 22:21:26 -0700

    completed lecture 76

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	state = { loggedIn: null };
+
 	componentWillMount() {
 		firebase.initializeApp({
-		apiKey: 'AIzaSyDF0vwz2hGjWgy22gzyA0XB_EzbmMlB_sc',
-		authDomain: 'authentication-rnc9.firebaseapp.com',
-		databaseURL: 'https://authentication-rnc9.firebaseio.com',
-		projectId: 'authentication-rnc9',
-		storageBucket: 'authentication-rnc9.appspot.com',
-		messagingSenderId: '320056014461'
+			apiKey: 'AIzaSyDF0vwz2hGjWgy22gzyA0XB_EzbmMlB_sc',
+			authDomain: 'authentication-rnc9.firebaseapp.com',
+			databaseURL: 'https://authentication-rnc9.firebaseio.com',
+			projectId: 'authentication-rnc9',
+			storageBucket: 'authentication-rnc9.appspot.com',
+			messagingSenderId: '320056014461'
   	});
+
+		firebase.auth().onAuthStateChanged((user) => {
+				if (user) {
+					this.setState({ loggedIn: true });
+				} else {
+					this.setState({ loggedIn: false });
+				}
+		});
+
+	} //componentWillMount
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return (<CardSection><Button>Log Out</Button></CardSection>);
+			case false:
+				return <LoginForm />;
+			default:
+				return (<CardSection><Spinner size="large" /></CardSection>);
+		}
 	}
+
 
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent() }
 			</View>
 			);
 	}
