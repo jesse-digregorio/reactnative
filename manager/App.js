50de9464ddd6313771e8a0c37808a456fb795374
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-08 15:19:54 -0700

    lesson 108, firebase is now set up.

--- manager/App.js
+++ manager/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './src/reducers';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBo35Bi3jjTvnvPbkB7knuJ5YKjLwIUKK8',
+      authDomain: 'manager-rnc9.firebaseapp.com',
+      databaseURL: 'https://manager-rnc9.firebaseio.com',
+      projectId: 'manager-rnc9',
+      storageBucket: 'manager-rnc9.appspot.com',
+      messagingSenderId: '297389060800'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Text>
+            Marmalade!
+          </Text>
+        </View>
+      </Provider>
     );
   }
 }
