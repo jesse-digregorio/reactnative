97b1a7c37ef4170deb17b3a702ac8a5ccb34f8dd
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-08 15:40:57 -0700

    Lecture 110 - LoginForm layout

--- manager/App.js
+++ manager/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
 
 export default class App extends React.Component {
   componentWillMount() {
@@ -21,10 +22,8 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View style={styles.container}>
-          <Text>
-            Marmalade!
-          </Text>
+        <View>
+          <LoginForm />
         </View>
       </Provider>
     );
