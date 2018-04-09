772680901c46eba391d1de4d420c8aeee19b3c7e
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-08 19:37:29 -0700

    logging in user with an async call, and manual dispatch with redux-thunk

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,7 +1,8 @@
-import firebase from firebase;
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
   } from './types';
 
 export const emailChanged = (text) => {
@@ -18,9 +19,13 @@ export const passwordChanged = (text) => {
   };
 };
 
-export const loginUser = ({ email, password }) {
+export const loginUser = ({ email, password }) => {
+
   return (dispatch) => {
-    firebase.auth.signInWithEmailAndPassword(email, password)
-      .then(user => console.log(user));
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      });
+
   };
 };
