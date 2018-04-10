c529c149c34450d15da9372dd0ca8d42de056a27
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-09 21:59:20 -0700

    lecture 136 - dynamic property updates

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
@@ -9,13 +11,17 @@ class EmployeeCreate extends Component {
           <Input
             label="Name"
             placeholder="Jane Doe"
-            />
+            value={this.props.name}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+             />
         </CardSection>
 
         <CardSection>
           <Input
             label="Phone"
             placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
             />
         </CardSection>
 
@@ -33,4 +39,10 @@ class EmployeeCreate extends Component {
 
 } // class EmployeeCreate
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
