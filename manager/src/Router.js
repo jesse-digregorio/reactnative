67c1330a10d911b52e2b61d943278e6de1ff4346
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-09 21:08:17 -0700

    lecture 132 - scaffolding for Employee Create form, and scene transitions

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
-    <Router>
+    <Router sceneStyle={{ paddingTop: 65 }}>
     <Scene key="root" hideNavBar>
       <Scene key="auth">
         <Scene initial key="login" component={LoginForm} title="Please Login" />
       </Scene>
       <Scene key="main">
         <Scene
-          rightTitle="Add"
-          onRight={() => { console.log('right!!!') }}
           key="employeeList"
+          rightTitle="Add"
+          onRight={() => { Actions.employeeCreate() }}
           component={EmployeeList}
-          title="Employees" />
+          title="Employees"
+          initial
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee" />
       </Scene>
     </Scene>
     </Router>
