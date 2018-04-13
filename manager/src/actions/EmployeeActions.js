ef353bfd0b92cf7ac94fa6fa675d144ac10869d4
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-12 20:12:26 -0700

    lecture 145 - fetching data from firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -36,3 +37,14 @@ export const employeeCreate = ({ name, phone, shift }) => {
 //    use: .then(() => Actions.employeeList({ type: 'reset' }));
 // or
 //    a pop-up suggested: Actions.pop()
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
+  };
+};
