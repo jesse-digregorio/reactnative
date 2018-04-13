9945a05ed240c34e592d6fe468e5652b50b1fa83
Jesse DiGregorio <jesse.digregorio@nordstrom.com>
2018-04-12 19:47:32 -0700

    lecture 144 - resetting form properties

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -13,6 +15,24 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
 
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+  // according the the instructor, this is a little "dishonest".
+  // according to him, we don't really want or need to return anything form here,
+  // but he wrapped it in a function to satisfy 'redux-thunk' requirement.
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE })
+        Actions.pop());
+      };
+  };
 };
+// problem with .then(() => Actions.employeeList());
+// is that it puts us "back" on employee list, but not really.
+// It really puts "employeeList" on the 'Scene Stack', putting us
+// in the position of having a 'back arrow' to the Employee Create screen.
+// That's no good.
+// two solutions to this:
+//    use: .then(() => Actions.employeeList({ type: 'reset' }));
+// or
+//    a pop-up suggested: Actions.pop()
